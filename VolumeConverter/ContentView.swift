8789828c808752e27bcb5a3c32ebb19d82fc9ea6
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-02-05 19:17:52 -0800

    Added comments to clarify code

--- VolumeConverter/ContentView.swift
+++ VolumeConverter/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    // State variables to track user input and selected units
     @State private var inputUnit = "milliliters"
     @State private var outputUnit = "liters"
     @State private var userInput = 1.0
@@ -19,11 +20,12 @@ struct ContentView: View {
                  "pints": 473.17647274592001,
                  "gallons": 3785.411789132031272]
     
-    //    let units = ["ml", "l", "c", "pt", "gal"]
+    // Computed property to calculate the input value in milliliters
     var inputValueInMilliliters: Double {
         userInput * units[inputUnit]!
     }
     
+    // Computed property to calculate the converted output value
     var userOutput: Double {
         inputValueInMilliliters / units[outputUnit]!
     }
@@ -40,7 +42,6 @@ struct ContentView: View {
                     }
                     .pickerStyle(.menu)
                     
-                    
                     // Picker for output unit
                     Picker("Output Unit", selection: $outputUnit) {
                         ForEach(units.keys.sorted(), id: \.self) { unit in
@@ -55,7 +56,7 @@ struct ContentView: View {
                     TextField("Initial value", value: $userInput, format: .number)
                 }
                 
-                //Display converted value
+                // Display converted value
                 Section("Output") {
                     Text(userOutput.formatted() + " \(outputUnit)")
                 }
