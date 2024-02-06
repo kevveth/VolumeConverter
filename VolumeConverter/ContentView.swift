d28a317052dda5ebf5c47e195899a034ba93f58e
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-02-05 19:03:51 -0800

    Initial Commit

---
+++ VolumeConverter/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  VolumeConverter
+//
+//  Created by Kenneth Oliver Rathbun on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var inputUnit = "milliliters"
+    @State private var outputUnit = "liters"
+    @State private var userInput = 1.0
+    
+    // Units and their conversion rates to milliliters
+    let units = ["milliliters": 1.0,
+                 "liters": 1000,
+                 "cups": 236.5882365,
+                 "pints": 473.17647274592001,
+                 "gallons": 3785.411789132031272]
+    
+    //    let units = ["ml", "l", "c", "pt", "gal"]
+    var inputValueInMilliliters: Double {
+        userInput * units[inputUnit]!
+    }
+    
+    var userOutput: Double {
+        inputValueInMilliliters / units[outputUnit]!
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                // Picker for input unit
+                Section {
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(units.keys.sorted(), id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    
+                    
+                    // Picker for output unit
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach(units.keys.sorted(), id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                }
+                
+                // Accept user input for value to convert
+                Section("Input") {
+                    TextField("Initial value", value: $userInput, format: .number)
+                }
+                
+                //Display converted value
+                Section("Output") {
+                    Text(userOutput.formatted() + " \(outputUnit)")
+                }
+            }
+            .navigationTitle("↔️ Volume Converter")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VolumeConverter/VolumeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeConverterApp.swift
+//  VolumeConverter
+//
+//  Created by Kenneth Oliver Rathbun on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct VolumeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
